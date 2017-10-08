ff336b233edc76331b5732dedadaf69a8ac40b1c
mike3630 <mike_smith_356@hotmail.com>
2017-10-07 23:18:13 -0400

    add test data condition when api is not accessible, finished first try of sliding window

--- WeatherPrediction.js
+++ WeatherPrediction.js
@@ -4,10 +4,45 @@ let getDataPromise = require("./DataCollection.js").getDataPromise;
 async function GetWeatherData(){
     let currWeather = [];
     let lastYear = [];
-    await getDataPromise(currWeather, 0);
-    await getDataPromise(lastYear, 1);
-    console.log(currWeather);console.log();
-    console.log(lastYear);console.log();console.log();console.log();
+    let test = 0;
+
+    if (test === 1){
+        await getDataPromise(currWeather, 0);
+        await getDataPromise(lastYear, 1);
+    }
+    else {
+
+        currWeather = [ { maxTemp: 14,
+                            minTemp: 2,
+                            humidity: 0.69,
+                            rainfall: 0.6705599999999999 },
+                        { maxTemp: 17, minTemp: 4, humidity: 0.73, rainfall: 0.18288 },
+                        { maxTemp: 20, minTemp: 7, humidity: 0.72, rainfall: 0.06096 },
+                        { maxTemp: 23, minTemp: 13, humidity: 0.7, rainfall: 0.06096 },
+                        { maxTemp: 22, minTemp: 10, humidity: 0.81, rainfall: 8.2296 },
+                        { maxTemp: 18, minTemp: 10, humidity: 0.78, rainfall: 0.06096 },
+                        { maxTemp: 19, minTemp: 9, humidity: 0.76, rainfall: 0.06096 } ];
+
+        lastYear = [ { maxTemp: 16, minTemp: 4, humidity: 0.79, rainfall: 3.23088 },
+                        { maxTemp: 16, minTemp: 3, humidity: 0.69, rainfall: 0.06096 },
+                        { maxTemp: 16, minTemp: 2, humidity: 0.69, rainfall: 0.06096 },
+                        { maxTemp: 16, minTemp: 8, humidity: 0.75, rainfall: 0.06096 },
+                        { maxTemp: 19, minTemp: 9, humidity: 0.75, rainfall: 1.28016 },
+                        { maxTemp: 19, minTemp: 8, humidity: 0.82, rainfall: 0 },
+                        { maxTemp: 17, minTemp: 9, humidity: 0.73, rainfall: 0 },
+                        { maxTemp: 17, minTemp: 10, humidity: 0.67, rainfall: 0 },
+                        { maxTemp: 17, minTemp: 12, humidity: 0.69, rainfall: 0 },
+                        { maxTemp: 15, minTemp: 12, humidity: 0.94, rainfall: 3.59664 },
+                        { maxTemp: 19,
+                            minTemp: 7,
+                            humidity: 0.84,
+                            rainfall: 2.3774399999999996 },
+                        { maxTemp: 19, minTemp: 7, humidity: 0.78, rainfall: 0.06096 },
+                        { maxTemp: 20, minTemp: 7, humidity: 0.84, rainfall: 0 },
+                        { maxTemp: 21, minTemp: 7, humidity: 0.85, rainfall: 0 } ];
+    }
+    //console.log(currWeather);console.log();
+    //console.log(lastYear);console.log();console.log();console.log();
     slidingWindow(currWeather, lastYear);
 }
 
@@ -35,28 +70,25 @@ function slidingWindow(currWeather, lastYear){
 
     // Step 3
     buildWindows(windows, lastYear);
-    console.log(windows);
+    //console.log(windows);
 
     // Step 4
     let distances = [];
-    for (let i = 0; i < windows.length; i++){
-        let highTemp = 0;
-        let lowTemp = 0;
-        let humidity = 0;
-        let rainfall = 0;
-        for (let j = 0; j < windows[i].length; j++){
-            highTemp += windows[i][j]['maxTemp'];
-            lowTemp += windows[i][j]['minTemp'];
-            humidity += windows[i][j]['humidity'];
-            rainfall += windows[i][j]['rainfall'];
-        }
-        /*highTemp = highTemp/windows[0].length;
-        lowTemp = lowTemp/windows[0].length;
-        humidity = humidity/windows[0].length;
-        rainfall = rainfall/windows[0].length;*/
-        console.log(highTemp)        
-    }
+    EuclideanDistances(distances, windows, currWeather);
+    //console.log(distances)
 
+    // Step 5
+    let conditions = ['maxTemp', 'minTemp', 'humidity', 'rainfall'];
+    for (let i = 0; i < conditions.length; i++){
+        let index = minED(distances, conditions[i]);
+        //console.log(distances[index][conditions[i]]);
+        //console.log(windows[index])
+
+        // Step 6
+        let variation = weatherVariation(windows, currWeather, index, conditions[i]);
+
+        console.log("predicted " + conditions[i] + ": " +  (currWeather[6][conditions[i]] + variation));
+    }
 }
 
 //splits the historical weather into the respective windows
@@ -69,4 +101,58 @@ function buildWindows(windows, lastYear){
         windows.push(arr);
     }
 
+}
+
+// finds the ED of each value, min/max temp humidity and rainfall
+function EuclideanDistances(distances, windows, currWeather){
+    for (let i = 0; i < windows.length; i++){
+        let maxTemp = 0;
+        let minTemp = 0;
+        let humidity = 0;
+        let rainfall = 0;
+        for (let j = 0; j < windows[i].length; j++){
+            maxTemp += Math.pow(windows[i][j]['maxTemp'] - currWeather[j]['maxTemp'], 2);
+            minTemp += Math.pow(windows[i][j]['minTemp'] - currWeather[j]['minTemp'], 2);
+            humidity += Math.pow(windows[i][j]['humidity'] - currWeather[j]['humidity'], 2);
+            rainfall += Math.pow(windows[i][j]['rainfall'] - currWeather[j]['rainfall'], 2);
+        }
+        maxTemp = Math.sqrt(maxTemp);
+        minTemp = Math.sqrt(minTemp);
+        humidity = Math.sqrt(humidity);
+        rainfall = Math.sqrt(rainfall);
+        distances.push({"maxTemp":maxTemp,
+         "minTemp":minTemp,
+         "humidity":humidity,
+         "rainfall":rainfall})      
+    }
+
+}
+
+// finds min ED from a list of EDs
+function minED(distances, param){
+    let min = 10000;
+    let index = 0;
+    for (let i = 0; i < distances.length; i++){
+        if (distances[i][param] < min){
+            min = distances[i][param];
+            index = i;
+        }
+    }
+    return index;
+}
+
+function weatherVariation(windows, currWeather, index, param){
+    let meanCurr = 0;
+    let meanLast = 0;
+    for (let i = 0; i < 6; i++){
+        meanCurr += currWeather[i+1][param] - currWeather[i][param];
+        meanLast += windows[index][i+1][param] - windows[index][i][param];
+    }
+
+    meanCurr = meanCurr/6;
+    meanLast = meanLast/6;
+
+    let variation = (meanCurr + meanLast)/2;
+
+    return variation;
 }
